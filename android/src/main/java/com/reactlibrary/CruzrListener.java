78a467cf26cf182f35457c671a4b1bb6bd47b4dc
chenyongjian <yongjian.chen@b-and-qchina.com>
2018-06-27 16:40:28 +0800

    语音引擎初始化

---
+++ android/src/main/java/com/reactlibrary/CruzrListener.java
@@ -0,0 +1,15 @@
+package com.reactlibrary;
+
+/**
+ * @Description 回调接口
+ * @Author yongjian.chen
+ * @Date 2018/6/27
+ * @Version
+ */
+
+public interface CruzrListener {
+    /**
+     * 启动app
+     */
+    void startApp();
+}
---
+++ android/src/main/java/com/reactlibrary/CruzrUtils.java
@@ -0,0 +1,68 @@
+package com.reactlibrary;
+
+import android.content.Context;
+import android.content.Intent;
+import android.util.Log;
+
+import com.ubtechinc.cruzr.sdk.speech.ISpeechContext;
+import com.ubtechinc.cruzr.sdk.speech.SpeechRobotApi;
+import com.ubtechinc.cruzr.serverlibutil.interfaces.InitListener;
+
+import org.json.JSONObject;
+
+/**
+ * @Description
+ * @Author yongjian.chen
+ * @Date 2018/6/27
+ * @Version
+ */
+
+public class CruzrUtils {
+    /**
+     * 语音引擎--初始化
+     * @param context
+     * @param listener
+     */
+    public static void init(Context context,final CruzrListener listener){
+        SpeechRobotApi.get().initializ(context, 1200,new InitListener() { @Override
+        public void onInit() { //初始化成功 注册语音
+            SpeechRobotApi.get().registerSpeech(new ISpeechContext() {
+                @Override
+                public void onStart() {
+
+                }
+
+                @Override
+                public void onStop() {
+
+                }
+
+                @Override
+                public void onPause() {
+
+                }
+
+                @Override
+                public void onResume() {
+
+                }
+
+                @Override
+                public void onResult(String s) {
+                    Log.i("dan", "support onResult:" + s);
+                    try {
+                        JSONObject object = new JSONObject(s);
+                        String intentStr = object.optString("intent");
+                        if(intentStr.contains("购物")||intentStr.contains("导购")||intentStr.contains("导购屏")||intentStr.contains("")) {
+                            listener.startApp();
+                        }
+                    }catch (Exception e){
+                        e.printStackTrace();
+                    }
+
+
+                }
+            });
+        } });
+    }
+}
